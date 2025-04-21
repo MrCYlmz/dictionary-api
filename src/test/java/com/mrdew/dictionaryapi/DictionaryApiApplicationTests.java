b140061a5b81a14af6d9a3405cce91204091a790
MrCYlmz <celal63159@gmail.com>
2025-04-21 18:59:13 +0200

    initialize dictionary-api project with Spring Boot and PostgreSQL setup

---
+++ src/test/java/com/mrdew/dictionaryapi/DictionaryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrdew.dictionaryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DictionaryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
